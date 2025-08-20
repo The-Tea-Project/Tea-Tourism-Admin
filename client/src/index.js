01400a245686851d030e68a2dd55d3f402faaa25
Somesh Ghosh <someshghoshjoyguru@gmail.com>
2025-08-21 03:04:51 +0530

    wtf

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { ClerkProvider } from '@clerk/clerk-react';
 import App from './App';
 
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
+const clerkPubKey = 'pk_test_YW1wbGUtbWl0ZS0xNy5jbGVyay5hY2NvdW50cy5kZXYk'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ClerkProvider publishableKey={clerkPubKey}>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { ClerkProvider } from '@clerk/clerk-react';
 import App from './App';
 
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const clerkPubKey = 'pk_test_YW1wbGUtbWl0ZS0xNy5jbGVyay5hY2NvdW50cy5kZXYk'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
